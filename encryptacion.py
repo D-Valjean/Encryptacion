3f69b9019e7c46c26f2a10f043f7ee58370fbbbb
Rafael Franklin <131192250+D-Valjean@users.noreply.github.com>
2024-02-11 15:36:15 -0400

    Update encryptacion.py

--- encryptacion.py
+++ encryptacion.py
@@ -8,7 +8,7 @@ def write_key():
 
 def load_key():
     # ruta donde cargara la llave de encryptacion
-    file = open("C:\\key.key", "rb")
+    file = open("C:\\key2.key", "rb")
     key = file.read()
     file.close()
     return key
@@ -19,37 +19,39 @@ fer = Fernet(key)
 
 
 def view():
-    dictory_view = 'Z:\\Vault\\passwords.md'  # directorio para ver instancia
+    dictory_view = 'Z:\\Vault\\passwords2.md'  # directorio para ver instancia
     with open(dictory_view, 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            user, passw = data.split("|")
-            print("User:", fer.decrypt(user.encode()).decode(), "| Password:",
+            user, msjs, passw = data.split("|")
+            print("User:", fer.decrypt(user.encode()).decode(), "| Mensaje:", fer.decrypt(msjs.encode()).decode(), "| Password:",
                   fer.decrypt(passw.encode()).decode()+"\n")
 
 
 def add():
     # directorio para crear/agregar instancia
-    dictory_add = 'Z:\\Vault\\passwords.md'
+    dictory_add = 'Z:\\Vault\\passwords2.md'
     name = input('Account Name: ')
+    msjs = input('>_: ')
     pwd = input("Password: ")
-
     encrypted_name = fer.encrypt(name.encode()).decode()
+    encrypted_msjs = fer.encrypt(msjs.encode()).decode()
     encrypted_pwd = fer.encrypt(pwd.encode()).decode()
 
     with open(dictory_add, 'a') as f:
-        f.write(encrypted_name + "|" + encrypted_pwd + "\n")
+        f.write(encrypted_name + "|" + encrypted_msjs +
+                "|" + encrypted_pwd + "\n")
 
 
 while True:
     mode = input("\n" +
-                 "Escoja una opcion a Realizar (ver, agregar). \nPara salir escribe (q) \n>_").lower()
+                 "Escoja una opcion a Realizar (ver, add). \nPara salir escribe (q) \n>_ ").lower()
     if mode == "q":
         break
 
     if mode == "ver":
         view()
-    elif mode == "agregar":
+    elif mode == "add":
         add()
     else:
         print("Comando invalido.")
